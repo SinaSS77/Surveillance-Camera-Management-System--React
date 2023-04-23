3ff8e4ea7abbdf505886710abe0eaeffedfb037d
Sina Molaverdikhani <s.molaverdikhani@gmail.com>
2023-04-22 17:21:57 -0700

    Designed main page

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.scss';
+import Navbar from './components/navbar/Navbar';
+import CameraCard from './components/cards/Cards';
+import CameraView from './components/cameraView/CameraView'
+
+
+function App() {
+  return (
+    <>
+  {/* <Navbar></Navbar>
+  <CameraCard></CameraCard> */}
+  <CameraView></CameraView>
+  </>
+  );
+}
+
+export default App;
---
+++ client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import profileImage from "../../assets/profile.png";
+
+const Navbar = () => {
+
+
+  return (
+   
+      <div className=" w-full flex items-center my-auto py-5 fixed top-0 z-20 justify-between bg-slate-500">
+        <div >
+          <h1 className=" text-[2rem] text-cyan-500 items-center  pl-20"> Surveillance Camera Management System</h1>
+        </div>
+
+
+        <div className="flex gap-5 pr-10">
+
+          <img 
+          className=" w-12 h-12 "
+            src={profileImage}
+            alt=""
+          />
+          <div >
+
+            <div className=" gap-4 text-[1.2rem] ">
+              <label className=" m-10 ml-0">sina@sina.com</label>
+              <label className="  text-red-600 m-10">Logout</label>
+            </div>
+          </div>
+        </div>
+      </div>
+    
+  );
+};
+
+export default Navbar;
